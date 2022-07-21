2e34ba290afe601f8e26754392732d531982c78b
Yokke <mdembelepro@gmail.com>
2022-07-21 09:16:55 +0200

    Separating frontend from backend

---
+++ frontend/src/router/index.js
@@ -0,0 +1,25 @@
+// import { createRouter, createWebHistory } from 'vue-router'
+// import Konoha from '../views/Konoha.vue'
+
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'konoha',
+//     component: Konoha
+//   },
+//   {
+//     path: '/about',
+//     name: 'about',
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+//   }
+// ]
+
+// const router = createRouter({
+//   history: createWebHistory(process.env.BASE_URL),
+//   routes
+// })
+
+// export default router
